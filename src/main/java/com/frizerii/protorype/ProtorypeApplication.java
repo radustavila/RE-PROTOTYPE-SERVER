cfdf6faf667d6454151bf20904a8bcecbb945813
radustavila <stavila.radu@yahoo.com>
2022-01-08 16:11:03 +0200

    set

--- src/main/java/com/frizerii/protorype/ProtorypeApplication.java
+++ src/main/java/com/frizerii/protorype/ProtorypeApplication.java
@@ -2,6 +2,7 @@ package com.frizerii.protorype;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class ProtorypeApplication {
--- src/main/java/com/frizerii/protorype/repository/UserRepository.java
+++ src/main/java/com/frizerii/protorype/repository/UserRepository.java
@@ -2,8 +2,10 @@ package com.frizerii.protorype.repository;
 
 import com.frizerii.protorype.entity.UsersEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
+@Component
 @Repository
 public interface UserRepository extends JpaRepository<UsersEntity, Long> {
 
